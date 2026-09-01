8c58a32b98c6917f6e64621927b7753f42aa6cfd
Burak0153 <but001@edu.zealand.dk>
2026-09-01 10:39:19 +0200

    Update Cortado.cs

--- CoffeeShopConsoleAppNet60/Cortado.cs
+++ CoffeeShopConsoleAppNet60/Cortado.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CoffeeShopConsoleAppNet60
+﻿public class Cortado : Coffee
 {
-    internal class Cortado
+    public Cortado() : base (25)
     {
+
     }
 }
